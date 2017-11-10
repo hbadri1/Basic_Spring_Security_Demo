c78179f3d0e94e32b0dee185067c3197bb9c4ff1
hussembadri <badri.houssem@gmail.com>
2017-11-11 02:34:12 +0300

    Code

---
+++ src/main/java/com/hsm/SpringBootSecuredApplication.java
@@ -0,0 +1,12 @@
+package com.hsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecuredApplication.class, args);
+	}
+}
